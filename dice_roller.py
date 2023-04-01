21838ec6def0308eec4a3ac09b432b057face219
bruniin <brunovinicius2002@hotmail.com>
2023-04-01 18:35:44 -0300

    New icons

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,6 @@
 from tkinter import *
-#from PIL import Image, ImageTk
 
 corbg = "#101010"
-
 
 
 main = Tk()
@@ -13,8 +11,24 @@ main.iconphoto(False, PhotoImage(file = "./midia/dice_20_bg.png"))
 main.resizable(width = False, height = False)
 
 # Dice Icon on top
-img = PhotoImage(file=fr"./midia/dice_20_icon.png")
-d20_icon = Label(main, image=img, bg = corbg, compound="top", text="D&D Dice Roller", font=("Lucida Calligraphy", 18, "bold"), fg="white").pack(pady=15)
+d20_img = PhotoImage(file=fr"./midia/d20icon.png")
+d20_icon = Label(main, image=d20_img, bg = corbg, compound="bottom", text="D&D Dice Roller", font=("Lucida Calligraphy", 18, "bold underline"), fg="white").pack(pady=15)
+
+d4_img = PhotoImage(file=fr"./midia/d4.png")
+d4 = Button(main, relief="flat", image=d4_img, bg = corbg, compound="left", text="  1d4", font=("Berlin Sans FB", 15), fg="white").place(x=15, y=180)
+
+d4 = Button(main, relief="flat", image=d4_img, bg = corbg, compound="left", text="  1d6", font=("Berlin Sans FB", 15), fg="white").place(x=15, y=240)
+
+d4 = Button(main, relief="flat", image=d4_img, bg = corbg, compound="left", text="  1d8", font=("Berlin Sans FB", 15), fg="white").place(x=15, y=300)
+
+d4 = Button(main, relief="flat", image=d4_img, bg = corbg, compound="left", text="  1d10", font=("Berlin Sans FB", 15), fg="white").place(x=15, y=360)
+
+d4 = Button(main, relief="flat", image=d4_img, bg = corbg, compound="left", text="  1d12", font=("Berlin Sans FB", 15), fg="white").place(x=15, y=420)
+
+d4 = Button(main, relief="flat", image=d4_img, bg = corbg, compound="left", text="  1d20", font=("Berlin Sans FB", 15), fg="white").place(x=15, y=480)
+
+d4 = Button(main, relief="flat", image=d4_img, bg = corbg, compound="left", text="  1d100", font=("Berlin Sans FB", 15), fg="white").place(x=15, y=540)
+
 
 
 main.mainloop()
